3bb531de4e781c091198b5bcccf7dbf579b36e8a
pavan-koneti <pavan.koneti@tigeranalytics.com>
2021-02-01 23:21:47 +0530

    Operator1.java

---
+++ Operator1.java
@@ -0,0 +1,19 @@
+package first;
+
+public class Operator1 {
+    static void printname(){
+        System.out.println("tirupthi");
+    }
+    static boolean getname(){
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(10*10/5+3-1*4/2);
+        System.out.println(2+4);
+        System.out.println(10<<2);
+        System.out.println(2>3||3>2);
+        System.out.println(2>3|3>2);
+        System.out.println(10>>2);
+    }
+}
